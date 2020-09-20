3080294174529340cca3d87be34fd6e69b04dd68
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-09-20 18:16:50 -0300

    s9 webpack - parte 2

--- Section 9 - JavaScript Tooling e ES6 Modules/Webpack - Boilerplate/webpack.config.js
+++ Section 9 - JavaScript Tooling e ES6 Modules/Webpack - Boilerplate/webpack.config.js
@@ -1,12 +1,25 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "development", //production, a cada troca reiniciar npm run gera
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "public", "assets", "js"),
     filename: "bundle.js",
   },
-  module: {},
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+          },
+        },
+      },
+    ],
+  },
   devtool: "source-map",
 };
