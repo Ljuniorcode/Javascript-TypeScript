794f537a59e15c5fb1ba09ffd6531b7b9213f2ea
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-09-28 21:33:07 -0300

    s10 - models/database

--- Section 10 - NodeJS/Express/src/controllers/homeController.js
+++ Section 10 - NodeJS/Express/src/controllers/homeController.js
@@ -1,3 +1,12 @@
+const HomeModel = require("../models/HomeModel");
+
+HomeModel.create({
+  titulo: "Titulo de testes 02",
+  descricao: "Descrição de teste 02.",
+})
+  .then((dados) => console.log(dados))
+  .catch((e) => console.log(e));
+
 exports.paginaInicial = (req, res) => {
   res.render("index");
 };
