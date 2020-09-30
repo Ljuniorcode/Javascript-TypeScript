5ed0b3bac4e40c9fbe481c61758b18b389319ed2
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-09-30 14:13:28 -0300

    s11 - corrigindo bug, criar e logar usuário

--- Section 11 - ProjetoAgenda/src/controllers/loginController.js
+++ Section 11 - ProjetoAgenda/src/controllers/loginController.js
@@ -1,6 +1,8 @@
 const Login = require('../models/LoginModel')
 
 exports.index = (req, res) => {
+  console.log(req.session.user)
+  if (req.session.user) return res.render('login-logado')
   res.render("login");
 };
 
@@ -26,3 +28,41 @@ exports.register = async function (req, res) {
     res.render('404')
   }
 };
+
+
+exports.login = async function (req, res) {
+  try {
+    const login = new Login(req.body)
+    await login.login()
+
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors)
+      req.session.save(function () {
+        return res.redirect('back')
+      })
+      return;
+    }
+
+    if (!login.user) {
+      req.flash('errors', login.errors)
+      req.session.save(function () {
+        return res.redirect('back')
+      })
+      return;
+    }
+
+    req.flash('success', 'Login feito com sucesso')
+    req.session.user = login.user;
+    req.session.save(function () {
+      return res.redirect('back')
+    })
+
+  } catch (e) {
+    res.render('404')
+  }
+};
+
+exports.logout = function (req, res) {
+  req.session.destroy()
+  res.redirect('/')
+}
